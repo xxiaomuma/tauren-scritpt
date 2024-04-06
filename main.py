5a142d8144b43a52be78e87410b404cb220ec4e2
18146695070 <m18870885528@163.com>
2024-04-06 12:55:50 +0800

    fix 界面友好

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ def select_douyin_function():
         print("抖音功能:")
         print("1.账户链接")
         print("2.视频链接")
+        print("3.返回上一级")
         function_num = input("请选择抖音功能:")
-        if function_num:
+        if function_num == "1" or function_num == "2":
             select_douyin_operation(function_num)
+        elif function_num == "3":
+            select_platform()
         else:
             print("输入有误")
 
@@ -36,19 +39,23 @@ def select_douyin_function():
 def select_douyin_operation(function_num):
     while True:
         print("抖音操作:")
-        print("1.简单模式")
-        print("2.评论模式")
+        print("1.简单评论模式")
+        print("2.评论区评论模式")
+        print("3.返回上一级")
         operation_num = input("请选择抖音操作:")
-        if operation_num:
+        if operation_num == "1" or operation_num == "2":
+            while True:
+                if not dou_yin.is_login:
+                    print("请先登录...")
+                    continue
+                break
             if function_num == "1":
                 dou_yin.search_account(operation_num)
             elif function_num == "2":
-                while True:
-                    if dou_yin.is_login:
-                        dou_yin.search_video(operation_num)
-                        break
+                dou_yin.search_video(operation_num)
+        elif operation_num == "3":
+            select_douyin_function()
         else:
             print("输入有误")
-
 
 select_platform()
